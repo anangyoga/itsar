29116a5e54da90180a98dc5df5d5039cf63bbc8e
anangyoga <aharwinto@gmail.com>
2025-11-29 00:10:50 +0700

    feat: soal tik algoritma

---
+++ app/data/semester-satu-tik.ts
@@ -0,0 +1,115 @@
+export default [
+  {
+    question: "Apa itu Algoritma?",
+    choices: [
+      "Algoritma adalah ilmu TIK",
+      "Algoritma itu berkaitan dengan komputer",
+      "Algoritma adalah cara",
+      "Algoritma adalah langkah untuk menyelesaikan kegiatan",
+    ],
+    answer: 3,
+  },
+  {
+    question: "Contoh Algoritma yang tepat adalah...",
+    choices: [
+      "Urutan menggoreng telur",
+      "Adik sudah bangun tidur",
+      "Gambar ikan",
+      "Garis titik titik",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Apa itu mengurai tugas?",
+    choices: [
+      "Membagi tugas ke teman-teman",
+      "Membagi tugas kelas",
+      "Membagi tugas besar menjadi tugas-tugas kecil",
+      "Membagi tugas ke orang lain",
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      "1. Buka pasta gigi | 2. Oles pasta gigi ke sikat gigi | 3. ...... | 4. Kumur-kumur. Langkah yang tepat untuk nomer 3 adalah?",
+    choices: ["Mandi", "Menggosok Gigi", "Cuci Muka", "Menutup Pintu"],
+    answer: 1,
+  },
+  {
+    question:
+      "Hita ingin membuat kue. Hita dapat melakukan tugas dengan lebih mudah dengan cara...",
+    choices: [
+      "Menyuruh ibu",
+      "Menunda tugasnya",
+      "Membuat garis",
+      "Membagi tugas menjadi tugas-tugas kecil",
+    ],
+    answer: 3,
+  },
+  {
+    question: "Langkah kegiatan harus ditulis secara?",
+    choices: ["Berurutan", "Acak", "Bebas", "Berubah"],
+    answer: 0,
+  },
+  {
+    question:
+      "Urutan langkah-langkah yang tepat untuk membuat roti lapis adalah...",
+    choices: [
+      "Beli roti. Beli selai. Makan",
+      "Siapkan roti. Siapkan mentega. Oles mentega",
+      "Siapkan 2 roti. Oleskan selai. Satukan kedua roti",
+      "Satukan roti. Makan. Oleskan selai",
+    ],
+    answer: 2,
+    image: "/img/toast.jpg",
+  },
+  {
+    question:
+      "Warna hijau, kuning, merah diletakkan dalam urutan yang sama berulang-ulang. Hal itu disebut dengan?",
+    choices: ["Pola", "Bendera", "Warna-warni", "Berurutan"],
+    answer: 0,
+    image: "/img/pola.jpg",
+  },
+  {
+    question:
+      "Lingkaran - Segitiga - Lingkaran - ... | Pola selanjutnya adalah...",
+    choices: ["Lingkaran", "Segitiga", "Persegi Panjang", "Trapesium"],
+    answer: 1,
+  },
+  {
+    question:
+      "Merah - Hijau - Biru - Merah - Hijau - ... | Pola selanjutnya adalah...",
+    choices: ["Kuning", "Merah", "Hijau", "Biru"],
+    answer: 3,
+  },
+  {
+    question: "Ada berapa siswa pada gambar ini?",
+    choices: ["5", "6", "7", "8"],
+    answer: 1,
+    image: "/img/anak.jpg",
+  },
+  {
+    question: "Ada berapa siswa perempuan pada gambar ini?",
+    choices: ["1", "2", "3", "4"],
+    answer: 3,
+    image: "/img/anak.jpg",
+  },
+  {
+    question: "Ada berapa siswa yang memakai kerudung pada gambar ini?",
+    choices: ["1", "2", "3", "4"],
+    answer: 0,
+    image: "/img/anak.jpg",
+  },
+  {
+    question: "Ada berapa siswa laki-laki pada gambar ini?",
+    choices: ["1", "2", "3", "4"],
+    answer: 1,
+    image: "/img/anak.jpg",
+  },
+  {
+    question: "Ada berapa siswa yang memakai dasi pada gambar ini?",
+    choices: ["6", "5", "4", "3"],
+    answer: 0,
+    image: "/img/anak.jpg",
+  },
+];
---
+++ app/pages/semester-satu-tik.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useQuiz } from "~/composables/useQuiz";
+import questions from "~/data/semester-satu-tik";
+import QuizStarts from "~/components/QuizStarts.vue";
+import QuizEnds from "~/components/QuizEnds.vue";
+
+const quiz = useQuiz();
+
+useHead({
+  title: "Soal TIK - Berpikir Komputasional",
+  meta: [
+    {
+      name: "Kumpulan soal tentang Berpikir Komputasional",
+      content: "Kumpulan soal tentang Berpikir Komputasional",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div class="max-w-xl mx-auto p-6 min-h-screen flex flex-col">
+    <div
+      class="override bg-white border border-black rounded-xl shadow-[4px_4px_#000] p-6 w-full flex-1"
+    >
+      <QuizStarts
+        v-if="!quiz.state.started"
+        :title="'Soal TIK 1'"
+        :questions="questions"
+        :quiz="quiz"
+      />
+
+      <QuizQuestions
+        v-else-if="quiz.state.started && !quiz.state.finished"
+        :quiz="quiz"
+      />
+
+      <QuizEnds
+        v-else
+        :score="quiz.state.score"
+        :questions="quiz.state.questions"
+        :quiz="quiz"
+      />
+    </div>
+  </div>
+</template>

726aaafd775994bcfb9e9eafce99b0d0910e295f
anangyoga <aharwinto@gmail.com>
2025-12-01 22:36:31 +0700

    feat: soal seni budaya

---
+++ app/data/semester-satu-seni-budaya.ts
@@ -0,0 +1,130 @@
+export default [
+  {
+    question: "Apa itu garis lurus?",
+    choices: [
+      "Garis yang lurus",
+      "Garis yang melengkung",
+      "Garis zigzag",
+      "Garis gelombang",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Bangun datar yang memiliki garis melengkung adalah...",
+    choices: ["Persegi", "Lingkaran", "Segitiga", "Bintang"],
+    answer: 1,
+  },
+  {
+    question: "Huruf yang memiliki bentuk mirip lingkaran adalah...",
+    choices: ["F", "O", "K", "Z"],
+    answer: 1,
+  },
+  {
+    question: "Huruf yang memiliki garis lurus dan garis lengkung adalah...",
+    choices: ["T", "L", "V", "D"],
+    answer: 3,
+  },
+  {
+    question: "Huruf yang memiliki garis lengkung adalah...",
+    choices: ["S", "A", "W", "N"],
+    answer: 0,
+  },
+  {
+    question: "Huruf yang memiliki garis zigzag adalah...",
+    choices: ["I", "O", "Z", "D"],
+    answer: 2,
+  },
+  {
+    question: "Hewan yang tubuhnya ada titik-titiknya adalah...",
+    choices: ["Ayam Krispi", "Kanguru", "Citah", "Gajah"],
+    answer: 2,
+  },
+  {
+    question: "Hewan yang tubuhnya ada garis-garisnya adalah...",
+    choices: ["Kelinci", "Jerapah", "Cicak", "Zebra"],
+    answer: 3,
+  },
+  {
+    question: "Bentuk bidang yang ada pada gambar tersebut adalah...",
+    choices: ["Lingkaran", "Persegi Panjang", "Segitiga", "Layang-layang"],
+    answer: 1,
+    image: "/img/monitor.webp",
+  },
+  {
+    question: "Benda yang memiliki bentuk seperti lingkaran adalah...",
+    choices: ["Gunung", "Tempat Tidur", "Papan Tulis", "Piring"],
+    answer: 3,
+  },
+  {
+    question: "Bunyi alam adalah bunyi yang berasal dari...",
+    choices: ["Alam", "Mesin", "Terompet", "Klakson"],
+    answer: 0,
+  },
+  {
+    question: "Bunyi buatan adalah...",
+    choices: [
+      "Bunyi petir",
+      "Bunyi yang dibuat oleh suara hewan",
+      "Bunyi air mengalir",
+      "Bunyi yang dibuat oleh manusia",
+    ],
+    answer: 3,
+  },
+  {
+    question: "Contoh bunyi alam adalah...",
+    choices: [
+      "Suara kodok dan bel sekolah",
+      "Bunyi mesin motor dan mobil",
+      "Suara ombak dan suara jangkrik",
+      "Suara gitar",
+    ],
+    answer: 2,
+  },
+  {
+    question: "Bunyi alam biasanya berasal dari",
+    choices: [
+      "Benda yang dipukul",
+      "Suara gesekan daun",
+      "Mesin yang dinyalakan",
+      "Terompet yang ditiup",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Apa itu irama?",
+    choices: [
+      "Suara yang keras",
+      "Bunyi yang berulang dan teratur",
+      "Bunyi yang dari alam",
+      "Suara buatan",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Suara peluit ditiup adalah...",
+    choices: ["dhung-dhung", "tek-tek-tek", "priiiit", "dor-dor-dor-dor"],
+    answer: 2,
+    image: "/img/prit.jpg",
+  },
+  {
+    question: "Suara pintu saat diketuk adalah...",
+    choices: ["tok-tok-tok", "ha-ha-ha", "gong-gong-gong", "jreeeng"],
+    answer: 0,
+  },
+  {
+    question: "Pilih binatang yang memiliki suara paling keras...",
+    choices: ["semut", "gajah", "ikan", "cicak"],
+    answer: 1,
+  },
+  {
+    question: "Pilih binatang yang tidak bersuara...",
+    choices: ["kuda", "singa", "semut", "ayam"],
+    answer: 2,
+  },
+  {
+    question: "Alat musik ini bunyinya...",
+    choices: ["tok-tok-tok", "ha-ha-ha", "gong-gong-gong", "jreeeng"],
+    answer: 3,
+    image: "/img/gitar.png",
+  },
+];

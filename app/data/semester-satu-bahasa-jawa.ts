c350a99ece2c52e78809589ce82aa21530fe954f
anangyoga <aharwinto@gmail.com>
2025-12-01 23:23:31 +0700

    feat: soal bahasa jawa

---
+++ app/data/semester-satu-bahasa-jawa.ts
@@ -0,0 +1,121 @@
+export default [
+  {
+    question: "Swarane Sapi...",
+    choices: ["Nguk nguk", "Naa naaa", "Mooo mooo", "Lhaa lhaa"],
+    answer: 2,
+  },
+  {
+    question: "Swarane Jangkrik...",
+    choices: [
+      "Krik krik krik",
+      "Kraak kraak kraak",
+      "Piyek piyek",
+      "Kruyuk kruyuk",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Swarane Jago...",
+    choices: ["Gludug gludug", "Dor dor dor", "Hahahaha", "Kukuyuruk"],
+    answer: 3,
+  },
+  {
+    question: "Swarane Bebek...",
+    choices: ["Klutuk klutuk", "Ler ler ler", "Wek wek wek", "Jreeeng"],
+    answer: 2,
+  },
+  {
+    question: "Dona lagi ... buku.",
+    choices: ["Nulis", "Maca", "Nutup", "Coret"],
+    answer: 1,
+    image: "/img/read.webp",
+  },
+  {
+    question: "Gatekna gambar iki. Tulisen aksara sing kurang. B-e-...-e-k",
+    choices: ["a", "b", "d", "y"],
+    answer: 1,
+    image: "/img/bebek.jpg",
+  },
+  {
+    question: "Gatekna gambar iki. Tulisen aksara sing kurang. ...-a-c-a-n",
+    choices: ["M", "W", "S", "B"],
+    answer: 0,
+    image: "/img/macan.png",
+  },
+  {
+    question: "Gatekna gambar iki. Tulisen aksara sing kurang. D-...-k-a-r",
+    choices: ["a", "u", "o", "i"],
+    answer: 2,
+    image: "/img/dokar.jpg",
+  },
+  {
+    question:
+      "Gatekna gambar iki. Tulisen aksara sing kurang. D-o-...-...-n-a-n",
+    choices: ["l-o", "l-i", "l-e", "l-a"],
+    answer: 3,
+    image: "/img/toycar.webp",
+  },
+  {
+    question: "Rakiten tembung iki nganti dadi tembung sing bener. r-t-o-i",
+    choices: ["orit", "tori", "roti", "rito"],
+    answer: 2,
+    image: "/img/toast.jpg",
+  },
+  {
+    question:
+      "Rakiten tembung iki nganti dadi tembung sing bener. n-s-a-i-a-g-r",
+    choices: ["garisan", "garinas", "sanriga", "naarsig"],
+    answer: 0,
+    image: "/img/ruler.jpg",
+  },
+  {
+    question: "'Menthod-Menthog' iku opo?",
+    choices: ["Kembang Gula", "Tembang Dolanan", "Swara", "Aja ditiru"],
+    answer: 1,
+  },
+  {
+    question: "Menthog kui senenge ngapa?",
+    choices: [
+      "Ndheprok ing kandhang",
+      "Turu ing kasur",
+      "Mabur dhuwur",
+      "Mlayu banter",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Menthog ora seneng apa?",
+    choices: ["Dolan adoh", "Mangan iwak", "Nyambut gawe", "Nyapu ngepel"],
+    answer: 2,
+  },
+  {
+    question: "Mlakune menthog gawe apa?",
+    choices: ["Lugu", "Guyu", "Turu", "Lunyu"],
+    answer: 1,
+  },
+  {
+    question: "Solahe menthog...",
+    choices: ["Sregep", "Nyenengake", "Aja turu", "Aja ditiru"],
+    answer: 3,
+  },
+  {
+    question: "Bocah sekolah kudu sregep apa?",
+    choices: ["Sinau", "Nyanyi", "Nesu", "Guyu"],
+    answer: 0,
+  },
+  {
+    question: "Sapa sing ora sregep sinau bakal?",
+    choices: ["Kesel", "Pinter", "Bodho", "Sekolah"],
+    answer: 2,
+  },
+  {
+    question: "Sregep kui artine?",
+    choices: ["Rajin", "Males", "Lucu", "Wagu"],
+    answer: 0,
+  },
+  {
+    question: "Sifat kesed aja?",
+    choices: ["Diguyu", "Ditiru", "Dipercaya", "Digawe"],
+    answer: 1,
+  },
+];
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -106,6 +106,16 @@ useHead({
         </h2>
         <p class="text-gray-600">Bentuk & Bunyi</p>
       </NuxtLink>
+
+      <NuxtLink
+        to="/semester-satu-bahasa-jawa"
+        class="override grid h-full overflow-hidden rounded-lg border border-black bg-white text-black no-underline transition-all duration-200 ease-in-out hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[3px_3px_#000] grid-rows-[auto_1fr] p-4"
+      >
+        <h2 class="text-2xl font-bold text-gray-800 mb-2">
+          ꦧ Soal Bahasa Jawa
+        </h2>
+        <p class="text-gray-600">Suara & Tembang Dolanan</p>
+      </NuxtLink>
     </div>
   </div>
 </template>
